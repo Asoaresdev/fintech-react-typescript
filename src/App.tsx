8f657058d892297397dbfbcfb703faba895ecba6
Andrea Soares <asoaresdev@gmail.com>
2023-10-02 17:07:29 -0300

    feat: configuração e useFetch

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import Header from "./Components/Header"
+import Sidenav from "./Components/Sidenav"
+import Resume from "./Pages/Resume"
+import "./Style.css"
+
+function App() {
+  return (
+
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resume />
+      </main>
+    </div>
+
+  )
+}
+
+export default App
